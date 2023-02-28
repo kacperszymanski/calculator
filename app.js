52eca236cc98ed454c7b808210274c9888655b1d
kacperszymanski <91732728+kacperszymanski@users.noreply.github.com>
2023-02-28 15:04:40 +0100

    add "+" operator and js code for second value

--- app.js
+++ app.js
@@ -1,5 +1,53 @@
 
-let displayValue = 0
+const DEFAULT_VALUE = 0
+
+let firstValue = DEFAULT_VALUE
+let secondValue = ''
+let operator = ''
+
+const displayLastOperation = document.getElementById('displayLastOperation')
+const displayCurrentOperation = document.getElementById('displayCurrentOperation')
+const clear = document.getElementById('clear')
+const numberButtons = document.querySelectorAll('[data-number]')
+const operatorButtons = document.querySelectorAll('[data-operator]')
+
+
+clear.onclick = () => clearDisplay()
+
+
+
+numberButtons.forEach((button) =>
+    button.addEventListener('click', () => appendNumber(button.textContent))
+)
+
+operatorButtons.forEach((button) =>
+    button.addEventListener('click', () => setOperation(button.textContent))
+)
+
+function setOperation(operation) {
+    operator = operation
+}
+
+
+// firstValue = document.getElementById('displayCurrentOperation').textContent
+// if (operation === '+') {
+//     displayLastOperation.textContent = `${firstValue} ${operation}`
+
+
+function operate(operation, firstValue, secondValue) { }
+
+function appendNumber(number) {
+    if (displayCurrentOperation.textContent === '0') {
+        displayCurrentOperation.textContent = ''
+    }
+    displayCurrentOperation.textContent += number
+}
+
+
+function clearDisplay() {
+    firstValue = DEFAULT_VALUE
+    displayCurrentOperation.textContent = DEFAULT_VALUE
+}
 
 function add(a, b) {
     return a + b
@@ -22,4 +70,8 @@ function operate(operator, firstNum, secondNum) {
     else if (operator === 'substract') { substract(firstNum, secondNum) }
     else if (operator === 'multiply') { multiply(firstNum, secondNum) }
     else if (operator === 'divide') { divide(firstNum, secondNum) }
+}
+
+window.onload = () => {
+
 }
