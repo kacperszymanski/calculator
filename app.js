0c45b4c03a1ff6f98f72ee05aa3c2c6a82420033
kacperszymanski <akacperszymanski@gmail.com>
2023-03-09 22:35:13 +0100

    add error handler, keyboard shortcuts

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-
 const DEFAULT_VALUE = 0;
 
 let currentNumber = DEFAULT_VALUE;
@@ -21,6 +20,7 @@ deleteButton.addEventListener('click', deleteDigit);
 numberButtons.forEach(button => button.addEventListener('click', () => appendDigit(button.textContent)));
 operatorButtons.forEach(button => button.addEventListener('click', () => setOperation(button.textContent)));
 decimalButton.addEventListener('click', () => appendDigit('.'));
+document.addEventListener('keydown', handleKeyDown);
 
 function setOperation(operator) {
     if (operationType && previousNumber) {
@@ -39,16 +39,26 @@ function operate(operator, operand1, operand2) {
         case '+': return parseFloat(operand1) + parseFloat(operand2);
         case '-': return parseFloat(operand1) - parseFloat(operand2);
         case 'x': return parseFloat(operand1) * parseFloat(operand2);
-        case '÷': return parseFloat(operand1) / parseFloat(operand2);
+        case '÷': 
+            if (parseFloat(operand2) === 0) {
+                return 'ERROR';
+            }
+            return parseFloat(operand1) / parseFloat(operand2);
         default: return DEFAULT_VALUE;
     }
 }
 
+
 function calculate() {
     if (operationType && previousNumber) {
         const result = operate(operationType, previousNumber, currentNumber);
-        displayCurrentOperation.textContent = result;
-        displayLastOperation.textContent = `${previousNumber} ${operationType} ${currentNumber} = ${result}`;
+        if (result === 'ERROR') {
+            displayCurrentOperation.textContent = 'ERROR';
+            displayLastOperation.textContent = `${previousNumber} ${operationType} ${currentNumber} = ERROR`;
+        } else {
+            displayCurrentOperation.textContent = result;
+            displayLastOperation.textContent = `${previousNumber} ${operationType} ${currentNumber} = ${result}`;
+        }
         previousNumber = DEFAULT_VALUE;
         currentNumber = result;
         operationType = '';
@@ -56,18 +66,28 @@ function calculate() {
 }
 
 function appendDigit(digit) {
-    if (displayCurrentOperation.textContent === '0') {
+    if (displayCurrentOperation.textContent === '0' && digit !== '.') {
         displayCurrentOperation.textContent = '';
     }
-    if (digit === '.' && currentNumber.includes('.')) {
-        return;
+    if (digit === '.' && !displayCurrentOperation.textContent.includes('.')) {
+        if (displayCurrentOperation.textContent === '') {
+            displayCurrentOperation.textContent = '0';
+        }
+        displayCurrentOperation.textContent += digit;
+    } else {
+        displayCurrentOperation.textContent += digit;
     }
-    currentNumber = displayCurrentOperation.textContent += digit;
+    currentNumber = parseFloat(displayCurrentOperation.textContent) || DEFAULT_VALUE;
 }
 
 function deleteDigit() {
-    displayCurrentOperation.textContent = displayCurrentOperation.textContent.slice(0, -1);
-    currentNumber = parseFloat(displayCurrentOperation.textContent) || DEFAULT_VALUE;
+    if (displayCurrentOperation.textContent.length <= 1) {
+        displayCurrentOperation.textContent = DEFAULT_VALUE;
+        currentNumber = DEFAULT_VALUE;
+    } else {
+        displayCurrentOperation.textContent = displayCurrentOperation.textContent.slice(0, -1);
+        currentNumber = parseFloat(displayCurrentOperation.textContent) || DEFAULT_VALUE;
+    }
 }
 
 function clearDisplay() {
@@ -77,3 +97,39 @@ function clearDisplay() {
     displayCurrentOperation.textContent = DEFAULT_VALUE;
     displayLastOperation.textContent = DEFAULT_VALUE;
 }
+
+function handleKeyDown(event) {
+    const key = event.key;
+    if (!isNaN(key)) {
+        appendDigit(key);
+    } else if (key === '.') {
+        appendDigit('.');
+    } else if (key === '+' || key === '-' || key === '*' || key === '/') {
+        setOperation(convertOperatorSymbol(key));
+    } else if (key === 'Enter' || key === '=') {
+        calculate();
+    } else if (key === 'Escape') {
+        clearDisplay();
+    } else if (key === 'Backspace') {
+        deleteDigit();
+    } else if (key === 'Delete') {
+        clearDisplay();
+    }
+}
+
+function convertOperatorSymbol(key) {
+    switch (key) {
+        case '+':
+            return '+';
+        case '-':
+            return '-';
+        case '*':
+            return 'x';
+        case '/':
+            return '÷';
+        default:
+            return '';
+    }
+}
+  document.addEventListener('keydown', handleKeyDown);
+  
